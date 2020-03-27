40d36d797744040b919a2dbc13312e688c1cdaf3
Bmt-Privat-limited <biswarup979@gmail.com>
2020-03-28 02:58:18 +0530

    fourth time

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+
+    $(".js--services-section").waypoint(function(direction){
+        if (direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    });
+
+    //MIXITUP(PORTFOLIO SECTION)
     var mixer =mixitup('.container');
 });
